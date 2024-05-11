9bf110e8ce861d684706cba136f0aca2981a12c4
mokshag27 <mokshagawada@gmail.com>
2024-05-11 17:28:58 +0530

    pull

--- src/app/home/all-categories/all-categories.component.ts
+++ src/app/home/all-categories/all-categories.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { CookieService } from 'ngx-cookie-service';
+import { HomeService } from '../home.service';
 
 @Component({
   selector: 'app-all-categories',
   templateUrl: './all-categories.component.html',
   styleUrls: ['./all-categories.component.css']
 })
-export class AllCategoriesComponent {
+
+export class AllCategoriesComponent 
+{
+    constructor(private route: ActivatedRoute, private cookieService: CookieService,private router: Router,private modalService: NgbModal,private homeservice:HomeService
+  ) {
+
+  }
+
 
 }
